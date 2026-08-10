f04e891f2e6c6ee89fc7de71dc365c00dc18f018
Linh Nguyen <nttlinh100205@gmail.com>
2026-08-10 10:02:04 +0700

    init(registration-service): registration with cross-service call

---
+++ course-services/src/main/java/com/example/courseservices/controller/InternalCourseController.java
@@ -0,0 +1,19 @@
+package com.example.courseservices.controller;
+import com.example.courseservices.dto.CourseDTO;
+import com.example.courseservices.service.CourseService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+@RestController
+@RequestMapping("/internal/courses")
+@RequiredArgsConstructor
+public class InternalCourseController {
+    private final CourseService courseService;
+    @PatchMapping("/{id}/reserve-seat")
+    public CourseDTO reserveSeat(@PathVariable Long id) {
+        return courseService.reserveSeat(id);
+    }
+    @PatchMapping("/{id}/release-seat")
+    public CourseDTO releaseSeat(@PathVariable Long id) {
+        return courseService.releaseSeat(id);
+    }
+}
